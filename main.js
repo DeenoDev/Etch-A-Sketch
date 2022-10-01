f5fcab82ab36edfb698a2101246084f93d99d957
DeenoDev <deyanoklaasen@outlook.com>
2022-10-01 16:03:57 +0200

    black, gray and random sketch colors added to buttons.

--- main.js
+++ main.js
@@ -1,9 +1,42 @@
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.griTemplateRows = "repeat(16, 1fr)";
+let color = "black";
 
-for ( let i =0; i < 256; i++) {
+function populateBoard(size) {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach(div => div.remove());
+board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+board.style.griTemplateRows = `repeat(${size}, 1fr)`;
+
+let amount = size * size;
+for ( let i =0; i < amount; i++) {
     let square = document.createElement("div");
-    square.style.backgroundColor = 'blue';
+    square.addEventListener('mouseover', colorSquare);
+    square.style.backgroundColor = 'white';
     board.insertAdjacentElement("beforeend", square);
-};
+  }
+}
+
+populateBoard(16);
+
+function changeSize(input){
+    if (input >=2 && input <=100){
+    populateBoard(input)
+  }
+  else {
+    console.log("Too many squares");
+  }
+}
+
+function colorSquare () {
+    if (color === 'random'){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }
+    else {
+        this.style.backgroundColor = color;
+    }
+}
+
+function changeColor(choice) {
+    color = choice;
+}
+
