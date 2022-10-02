436cdfdf31289cf1159d7d9ac1f334a7dece8eb9
DeenoDev <deyanoklaasen@outlook.com>
2022-10-02 18:15:32 +0200

    board functionailty completed using javascript. Styling is half way with CSS.

--- main.js
+++ main.js
@@ -20,13 +20,14 @@ for ( let i =0; i < amount; i++) {
 populateBoard(16);
 
 function changeSize(input){
-    if (input >=2 && input <=100){
-    populateBoard(input)
-  }
-  else {
-    console.log("Too many squares");
-  }
-}
+    if (input >= 2 && input <= 100){
+    document.querySelector('.error').style.display = 'none';
+    populateBoard(input);
+  } else {
+    document.querySelector(".error").style.display = "flex";
+  
+  };
+};
 
 function colorSquare () {
     if (click) {
@@ -37,23 +38,26 @@ function colorSquare () {
             this.style.backgroundColor = color;
         }
     }
-}
+};
 
 function changeColor(choice) {
     color = choice;
-}
+};
 
 function resetBoard() {
     let board = document.querySelector(".board");
     let squares = board.querySelectorAll("div");
     squares.forEach(div => div.style.backgroundColor = "white");
-}
+};
 
-document.querySelector('body').addEventListener('click', () => {
-    click = !click;
-    if(click) {
-        document.querySelector('.mode').textContent = "Mode: Coloring";
-    } else {
-        document.querySelector('.mode').textContent = "Mode: Not Coloring";
+document.querySelector('body').addEventListener('click', (e) => {
+    if (e.target.tagName != "BUTTON") {
+            click = !click;
+            if(click) {
+                document.querySelector('.mode').textContent = "Mode: Coloring";
+            } else {
+                document.querySelector('.mode').textContent = "Mode: Not Coloring";
+            }
+        }
     }
-});
+ );
